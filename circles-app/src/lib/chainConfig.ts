288bb62c568a8c8c2c18898c36c4606a9c074ab2
daniel <4954577+jaensen@users.noreply.github.com>
2024-05-22 05:21:18 +0200

    add the circles sdk packages and chain config

---
+++ circles-app/src/lib/chainConfig.ts
@@ -0,0 +1,9 @@
+import type {ChainConfig} from "@circles-sdk/sdk";
+
+export const chainConfig: ChainConfig = {
+    pathfinderUrl: 'https://pathfinder.aboutcircles.com',
+    circlesRpcUrl: "https://rpc.helsinki.aboutcircles.com",
+    v1HubAddress: "0x29b9a7fBb8995b2423a71cC17cf9810798F6C543",
+    v2HubAddress: "0xFFfbD3E62203B888bb8E09c1fcAcE58242674964",
+    migrationAddress: "0x0A1D308a39A6dF8972A972E586E4b4b3Dc73520f"
+};
